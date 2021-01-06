9eeba8622c0776368fa0d25030a2a6b53d2acf4e
fangmingdong <fangmingdong0224@gmail.com>
2021-01-06 11:40:41 +0800

    fix config

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,6 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
-      // {
-      //   src:
-      //     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
-      //   integrity:
-      //     'sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW',
-      //   crossorigin: 'anonymous',
-      //   body: true,
-      // },
       {
         src: '/js/bootstrap.bundle.min.js',
         body: true,
